61c76895c77f2bcb55ee587bdc72174592bea8bf
phat@minim1 <phat.tranducvn@gmail.com>
2023-07-01 22:04:04 -0700

    Set up Redux store

---
+++ src/store.ts
@@ -0,0 +1,18 @@
+import { configureStore } from "@reduxjs/toolkit";
+import globalReducer from "@states/index";
+import { setupListeners } from "@reduxjs/toolkit/query";
+// import { api } from "@states/api";
+
+export const store = configureStore({
+	reducer: {
+		global: globalReducer,
+		// [api.reducerPath]: api.reducer,
+	},
+	// middleware: (getDefault) => getDefault().concat(api.middleware),
+});
+setupListeners(store.dispatch);
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
